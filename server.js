12c3578ff3975f783ac950dc3a32419dab994161
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2024-12-17 18:13:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "https://www.ahmadiyamadarsa.com"
+}));
 require("dotenv").config();
 const userRouter = require("./router/user");
 const adminRouter = require("./router/admin");
