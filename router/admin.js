3a669b911b5fc6db044d4db301714e8c87a2f76d
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2024-12-24 17:18:26 +0530

    Update admin.js

--- router/admin.js
+++ router/admin.js
@@ -46,8 +46,6 @@ const upload = multer({
     limits: { fileSize: 1024 * 1024 * 10 }
 });
 
-const upload = multer({ storage: storage });
-
 // const storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
 //     cb(null, "./public");
