a4e01a65fc5e8b71afe6ad9b6eb1f48aabbf7180
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2024-12-24 16:41:14 +0530

    Update admin.js

--- router/admin.js
+++ router/admin.js
@@ -20,6 +20,16 @@ const contactC = require("../controller/contactcontroller");
 const allimageC = require("../controller/allimagecontroller");
 
 const multer = require("multer");
+const AWS = require("aws-sdk");
+const multerS3 = require("multer-s3");
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+
+const s3 = new AWS.S3();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
